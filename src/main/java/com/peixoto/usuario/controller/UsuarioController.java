aef6a914632b004a18d01705f80837621798f036
Pedro Peixoto <phnpeixoto07@gmail.com>
2026-02-24 22:57:37 -0300

    adicionado o metodo que salva dados de usuário

---
+++ src/main/java/com/peixoto/usuario/controller/UsuarioController.java
@@ -0,0 +1,25 @@
+package com.peixoto.usuario.controller;
+
+import com.peixoto.usuario.business.UsuarioService;
+import com.peixoto.usuario.business.dto.UsuarioDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/usuario")
+@RequiredArgsConstructor
+
+public class UsuarioController {
+
+    private final UsuarioService usuarioService;
+
+    @PostMapping
+    public UsuarioDTO salvaUsuario(@RequestBody UsuarioDTO usuarioDTO){
+    return ResponseEntity.ok(usuarioService.salvaUsuario(usuarioDTO));
+
+    }
+}
