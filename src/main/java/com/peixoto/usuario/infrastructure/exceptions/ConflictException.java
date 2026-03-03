c817448b922fd576db24ed06b4240e6e7cbdf516
Pedro Peixoto <phnpeixoto07@gmail.com>
2026-03-02 21:18:03 -0300

    adicionado metodo get, delete e login

---
+++ src/main/java/com/peixoto/usuario/infrastructure/exceptions/ConflictException.java
@@ -0,0 +1,15 @@
+package com.peixoto.usuario.infrastructure.exceptions;
+
+
+public class ConflictException extends RuntimeException{
+
+        public ConflictException(String mensagem){
+            super(mensagem);
+        }
+
+        public ConflictException(String mensagem, Throwable throwable){
+            super(mensagem);
+        }
+
+    }
+
